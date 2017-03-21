5d8ef229448b54c4df598ba3b3d353a2bc1fd245
Lora Armstrong <lora.s.armstrong@gmail.com>
2017-03-21 09:23:07 +0000

    Lag, group to individual, simulation num fns

--- src/witan/send/utils.clj
+++ src/witan/send/utils.clj
@@ -19,3 +19,42 @@
                                vec
                                (sort-by (apply juxt col-key))
                                ds/dataset)))
+
+(defn lag
+  "Data must be kept chronologically ordered in order to work correctly"
+  ([dataset col-key]
+   (-> dataset
+       (ds/to-map)
+       (col-key)
+       (#(cons 0 %))
+       (butlast)))
+  ([dataset col-key lag-amount]
+   (-> dataset
+       (ds/to-map)
+       (col-key)
+       (#(concat (repeat lag-amount 0) %))
+       (as-> d (drop-last lag-amount d)))))
+
+(defn groups-to-individuals
+  [dataset freq-col]
+  (let [number-rows (first (:shape dataset))
+        row-seq (range 0 number-rows)
+        freqs (ds/column dataset freq-col)
+        rows-to-select (into [] (reduce concat
+                                        (map (fn [freq val] (repeat freq val))
+                                             freqs row-seq)))]
+    (-> dataset
+        (ds/remove-columns [freq-col])
+        (ds/select-rows rows-to-select))))
+
+(defn add-simulation-numbers
+  [dataset num-simulations]
+  (let [number-rows (first (:shape dataset))
+        row-seq (range 0 number-rows)
+        rows-to-select (into [] (reduce concat
+                                        (map (fn [val] (repeat num-simulations val))
+                                             row-seq)))
+        sim-numbers (into [] (reduce concat (repeat num-simulations (range 1 (inc num-simulations)))))]
+    (-> dataset
+        (ds/select-rows rows-to-select)
+        (ds/add-column :sim-num sim-numbers))))
