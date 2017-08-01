9180188f5485eafdf6671be594b20841789e55c4
Henry Garner <henry@henrygarner.com>
2017-08-01 11:09:27 +0100

    Remove obsolete tests

--- test/witan/send/utils_test.clj
+++ test/witan/send/utils_test.clj
@@ -5,10 +5,3 @@
             [schema.core :as s]
             [witan.send.schemas :as sc]
             [witan.send.send-test :as st]))
-
-(def ds-for-matrix (st/get-individual-input :transition-matrix))
-
-(deftest full-trans-mat-test
-  (testing "The transition matrix is generated with the correct format"
-    (let [matrix-formatted (full-trans-mat sc/States [0 26] ds-for-matrix)]
-      (is (s/validate sc/TransitionMatrix matrix-formatted)))))
