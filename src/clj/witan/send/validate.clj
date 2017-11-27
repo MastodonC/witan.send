3762c67ee641f2e7a91aa7c13e4f69d653f16633
Seb Bailey <sebastian@mastodonc.com>
2017-11-27 17:09:01 +0000

    Updated output-charts

--- src/clj/witan/send/validate.clj
+++ src/clj/witan/send/validate.clj
@@ -80,7 +80,7 @@
 
 
 (defn get-transitions []
-  (-> (tu/csv-to-dataset "data/towerhamlets/transitions5.csv" sc/TransitionCounts) ds/row-maps))
+  (-> (tu/csv-to-dataset "data/tower_hamlets/Tower-Hamlets-SEND-inputs-2017-11-21/transitions.csv" sc/TransitionCounts) ds/row-maps))
 
 (defn get-model-transitions []
   (read-string (slurp "target/transitions.edn")))
