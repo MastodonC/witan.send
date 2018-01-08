dc3a6c33809f81d9b110d91b52debd4ef75de2c6
Seb Bailey <sebastian@mastodonc.com>
2018-01-08 11:30:34 +0000

    Removed non-test

--- test/witan/send/acceptance/workspace_test.clj
+++ test/witan/send/acceptance/workspace_test.clj
@@ -34,14 +34,13 @@
                          :contracts (p/available-fns (m/model-library))}
           workspace'    (s/with-fn-validation (wex/build! workspace))
           result        (apply merge (wex/run!! workspace' {}))]
-      (is (first result))
       (is (= #{:total-in-send-by-ay :total-in-send-by-ay-group :by-state :total-cost :total-in-send :total-in-send-by-need :total-in-send-by-setting}
              (-> result first keys set))))))
 
 (defn run! []
   (let [fixed-catalog (mapv #(if (= (:witan/type %) :input)
                                (add-input-params %)
-                               (assoc-in % [:witan/params :simulations] 10))
+                               (assoc-in % [:witan/params :simulations] 1000))
                             (:catalog m/send-model))
         workspace     {:workflow  (:workflow m/send-model)
                        :catalog   fixed-catalog
@@ -49,27 +48,3 @@
         workspace'    (s/with-fn-validation (wex/build! workspace))
         result        (apply merge (wex/run!! workspace' {}))]
     nil))
-
-(defn transitions [dataset]
-  (->> (ds/row-maps dataset)
-       (reduce (fn [coll {:keys [academic-year-1 setting-1 need-1 setting-2 need-2]}]
-                 (update coll [academic-year-1
-                               (states/state need-1 setting-1)
-                               (states/state need-2 setting-2)] (fnil + 0) 1)) {})))
-
-(defn sankey [f transitions]
-  (let [settings (-> (reduce (fn [coll [ay setting-1 setting-2]]
-                               (into coll [setting-1 setting-2]))
-                             #{}
-                             (keys transitions))
-                     (vec))
-        settings-count (count settings)
-        setting-index (into {} (map vector settings (range)))
-        links (->> (filter (fn [[[ay s1 s2] n]]
-                             (f ay)) transitions)
-                   (reduce (fn [coll [[ay s1 s2] n]]
-                             (update coll [s1 s2] (fnil + 0) n))
-                           {})
-                   (map (fn [[[s1 s2] n]]
-                          {:source (setting-index s1) :target (+ settings-count (setting-index s2)) :value (float n)})))]
-    (-> {:links links :nodes (map #(hash-map :name (name %)) (concat settings settings))})))
