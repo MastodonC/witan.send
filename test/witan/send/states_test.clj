305672528da3a8a17ed4c1b63709dc246bddaa69
Seb Bailey <sebastian@mastodonc.com>
2018-02-01 11:51:31 +0000

    Removed comments, tests point at correct file

--- test/witan/send/states_test.clj
+++ test/witan/send/states_test.clj
@@ -5,15 +5,8 @@
             [witan.send.schemas :as sc]
             [clojure.core.matrix.dataset :as ds]))
 
-#_(def valid-states
-    {1 #{:foo-foo :bar-foo :baz-bar} 2 #{:foo-foo :bar-bar} 3 #{:foo-bar}})
-
-#_(deftest can-move-test
-    (testing ""
-      (is (true? (can-move? valid-states 2 :foo-foo)))))
-
 (defn test-inputs []
-  {:valid-setting-academic-years ["data/Tower-Hamlets-input-2018-01-15/valid-setting-academic-years.csv" sc/ValidSettingAcademicYears]})
+  {:valid-setting-academic-years ["data/demo/valid-setting-academic-years.csv" sc/ValidSettingAcademicYears]})
 
 (deftest calculate-valid-year-settings-from-setting-academic-years-test
   (let [[path schema] (:valid-setting-academic-years (test-inputs))
