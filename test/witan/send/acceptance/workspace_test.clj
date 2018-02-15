fde2399b61f12f6e140be2c6e915e2602fca7553
mikeyford <mikethehard@gmail.com>
2018-02-15 15:52:21 +0000

    run-model args logging

--- test/witan/send/acceptance/workspace_test.clj
+++ test/witan/send/acceptance/workspace_test.clj
@@ -7,7 +7,8 @@
             [witan.send.states :as states]
             [witan.send.test-utils :as tu]
             [witan.workspace-api.protocols :as p]
-            [witan.workspace-executor.core :as wex]))
+            [witan.workspace-executor.core :as wex]
+            [witan.send.utils :as u]))
 
 (def inputs-path "demo/")
 
@@ -59,6 +60,14 @@
 
   :splice-ncy - sets a national curriculum year to ignore transitions of prior to :filter-transitions-from year (optional)"
   [{:keys [iterations output? transition-modifier transitions-file modify-transitions-from filter-transitions-from splice-ncy]}]
+  (u/reset-log)
+  (u/log-info (str "Input Data: " (clojure.string/replace inputs-path #"/" " ")))
+  (u/log-info (str "Number of iterations: " iterations))
+  (u/log-info (str "Transitions modifier: " (if (nil? transition-modifier) "None" transition-modifier)))
+  (u/log-info (str "Transitions file: " (if (nil? transitions-file) "None" transitions-file)))
+  (u/log-info (str "Modify transitions from: " (if (nil? modify-transitions-from) "None" modify-transitions-from)))
+  (u/log-info (str "Filter transitions from: " (if (nil? filter-transitions-from) "None" filter-transitions-from)))
+  (u/log-info (str "Splice NCY: " (if (nil? splice-ncy) "None" splice-ncy) "\n"))
   (let [file-input    (if (nil? transitions-file)
                         (test-inputs)
                         (assoc (test-inputs) :settings-to-change [(str "data/" inputs-path transitions-file) sc/SettingsToChange]))
