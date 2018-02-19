58c7359d6614255a99428119e209d611c137b5e8
mikeyford <mikethehard@gmail.com>
2018-02-19 17:04:11 +0000

    changes requested

--- src/clj/witan/send/report.clj
+++ src/clj/witan/send/report.clj
@@ -2,9 +2,12 @@
   (:require [clojure.java.io :as io]
             [clj-time.core :as t]
             [clj-time.format :as f]
-            [clojure.java.shell :as sh]))
+            [clojure.java.shell :as sh]
+            [clojure.string :as str]))
 
 (def send-report (atom []))
+
+(def send-report-file "target/SEND_Report.txt")
 
 (defn reset-send-report []
   (sh/sh "git" "fetch" "--tags")
@@ -19,6 +22,5 @@
   (swap! send-report conj (apply str messages)))
 
 (defn write-send-report []
-  (io/delete-file "target/SEND_Report.txt" :quiet)
-  (doseq [line @send-report]
-    (spit "target/SEND_Report.txt" (println-str line) :append true)))
+  (io/delete-file send-report-file :quiet)
+  (spit send-report-file (str/join "\n" @send-report) :append true))
