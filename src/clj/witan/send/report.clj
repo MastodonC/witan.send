52d3dad4d903ca1c13ac4cea70d84d4fe0f5f486
mikeyford <mikethehard@gmail.com>
2018-02-27 15:15:08 +0000

    update report ns with markdown

--- src/clj/witan/send/report.clj
+++ src/clj/witan/send/report.clj
@@ -7,16 +7,26 @@
 
 (def send-report (atom []))
 
-(def send-report-file "target/SEND_Report.txt")
+(def send-report-file "target/SEND_Report.md")
+
+(defn italic [string]
+  (str "_" string "_"))
+
+(defn bold [string]
+  (str "__" string "__"))
+
+(defn heading [string]
+  (str "## " string))
 
 (defn reset-send-report []
   (sh/sh "git" "fetch" "--tags")
-  (let [model-version (str "SEND Model " (:out (sh/sh "git" "describe" "--abbrev=0" "--tags")))
-        run-datetime (str "Run on " (f/unparse-local (f/formatter "YYYY-MM-dd") (t/today)) " at " (f/unparse (f/formatter "HH:mm") (t/now)))
+  (let [model-version (heading (str "SEND Model " (:out (sh/sh "git" "describe" "--abbrev=0" "--tags"))))
+        run-datetime (str (italic "Run on") " " (f/unparse-local (f/formatter "YYYY-MM-dd") (t/today)) " " (italic "at") " " (f/unparse (f/formatter "HH:mm") (t/now)))
         os-version (str (System/getProperty "os.name") " " (System/getProperty "os.version"))
         clj-version (str "Clojure " (clojure-version) ", JVM " (System/getProperty "java.vm.version"))
-        git-branch (str "Using branch: " (:out (sh/sh "git" "rev-parse" "--symbolic-full-name" "--abbrev-ref" "HEAD")) "\n")]
-    (reset! send-report [model-version run-datetime os-version clj-version git-branch])))
+        git-branch (str "Using branch: " (bold (str/join "" (drop-last (:out (sh/sh "git" "rev-parse" "--symbolic-full-name" "--abbrev-ref" "HEAD"))))))
+        git-commit (str "ID of last commit: " (str/replace (:out (sh/sh "git" "log" "--format=\"%H\"" "-n" "1")) "\"" ""))]
+    (reset! send-report [model-version run-datetime os-version clj-version git-branch git-commit])))
 
 (defn info [& messages]
   (swap! send-report conj (apply str messages)))
