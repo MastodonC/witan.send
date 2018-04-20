c8e6452001fd74320288b23a9297f8bec5bd01c1
Michael Ford <michae.ford@mastodonc.com>
2018-04-20 12:08:32 +0100

    working with old input lines commented out

--- test/witan/send/acceptance/workspace_test.clj
+++ test/witan/send/acceptance/workspace_test.clj
@@ -11,11 +11,11 @@
             [witan.send.report :as report]
             [clojure.string :as str]))
 
-(def inputs-path "demo/")
+(def inputs-path "version-A/")
 
 (defn test-inputs []
   {:settings-to-change ["data/demo/modify-settings.csv" sc/SettingsToChange]
-   :initial-send-population [(str "data/" inputs-path "send-population.csv") sc/SENDPopulation]
+   ;:initial-send-population [(str "data/" inputs-path "send-population.csv") sc/SENDPopulation]
    :transition-matrix [(str "data/" inputs-path "transitions.csv") sc/TransitionCounts]
    :population [(str "data/" inputs-path "population.csv") sc/PopulationDataset]
    :setting-cost [(str "data/" inputs-path "need-setting-costs.csv") sc/NeedSettingCost]
