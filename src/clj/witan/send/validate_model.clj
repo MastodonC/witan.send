c656fdd16749ab48c806138ffa3b7f3e87fac8ea
mikeyford <mfogeo@gmail.com>
2018-05-04 17:48:48 +0100

    WIP model runs for seperate folds

--- src/clj/witan/send/validate_model.clj
+++ src/clj/witan/send/validate_model.clj
@@ -1,14 +1,16 @@
 (ns witan.send.validate-model
   (:require [witan.send.test-utils :as tu]
             [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [witan.send.acceptance.workspace-test :refer [run-model]]))
 
 
 
-;; get files to validate on
-
-
+; TODOs
+;; setup function:
 ;;; write validations dir
+;;; write path/temp dir
+
 
 (defn csv-data->maps [csv-data]
   (map zipmap
@@ -30,26 +32,51 @@
       (doall
         (csv/read-csv reader)))))
 
-(defn get-unique-years [transitions]
+(defn get-validation-years [transitions]
   (->> (map :calendar-year transitions)
        (distinct)
-       (map #(Integer/parseInt %))))
+       (map #(Integer/parseInt %))
+       (drop-last)))
+
+(defn return-fold [bound year transitions]
+  (filter #(bound (Integer/parseInt (:calendar-year %)) year) transitions))
+
+(defn copy-if-exists [source-path destination-path file]
+  (let [source (str source-path file)]
+    (if (.exists (clojure.java.io/as-file source))
+      (io/copy (io/file source) (io/file (str destination-path file))))))
+
+(defn copy-input-files [input-path year]
+  (let [source-path (str "data/" input-path)
+        destination-path (str source-path "temp/" year "/")]
+    (.mkdir (java.io.File. destination-path))
+    (doseq [file ["modify-settings.csv" "population.csv" "need-setting-costs.csv" "valid-setting-academic-years.csv"]]
+      (copy-if-exists source-path destination-path file))))
+
+(defn move-target-files [year]
+  (doseq [file ["Output_Count.csv" "Output_AY_State.csv"]]
+    (io/copy (io/file (str "target/" file)) (io/file (str "validation/" year "_modelled_" file)))))
 
 
-(defn return-up-to-fold [year-limit transitions]
-  (filter #(<= (Integer/parseInt (:calendar-year %)) year-limit) transitions))
+(defn validate-fold [input-path year transitions settings]
+  (if (contains? settings :override-inputs-path)
+    (throw (IllegalArgumentException. "Overriding inputs will break model validation")))
+  (let [train (return-fold <= year transitions)
+        test (return-fold > year transitions)
+        train-settings (merge settings {:override-inputs-path (str input-path "temp/" year "/")})]
+    (copy-input-files input-path year)
+    (write-csv train (str "data/" input-path "temp/" year "/transitions.csv"))
+    (write-csv test (str "validation/test_" year ".csv"))
+    (run-model train-settings)
+    (move-target-files year)))
 
-(defn return-after-fold [year-limit transitions]
-  (filter #(> (Integer/parseInt (:calendar-year %)) year-limit) transitions))
 
 
 
 
 
-(defn split-and-test [year transitions]
-  (let [train (return-up-to-fold year transitions)
-        test (return-after-fold year transitions)]
-    (write-csv test (str "validate/test" year ".csv"))))
+;; move files to temp folder
+;; run model with temp files
 
 
 
@@ -60,8 +87,6 @@
   ;; run model with csvs
   ;; store results
 
-(defn split-into-folds [transitions]
-  (let [years (get-unique-years transitions)]))
 
 
 
