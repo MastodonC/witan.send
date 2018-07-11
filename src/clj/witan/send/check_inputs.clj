991931142ee9e5e51c8b2d65e875de617ed99f85
mikeyford <mfogeo@gmail.com>
2018-07-11 15:41:03 +0100

    check for valid years

--- src/clj/witan/send/check_inputs.clj
+++ src/clj/witan/send/check_inputs.clj
@@ -1,8 +1,10 @@
 (ns witan.send.check-inputs
-  (:require [witan.send.report :as report]))
+  (:require [witan.send.report :as r]))
+
 
 (defn repl-warn [& args]
   (println (str "\u001B[1;31m" (apply str args) "\u001B[m")))
+
 
 (defn cost-for-state? [state setting-cost]
   "Takes map containing state and returns true for match in setting-cost"
@@ -10,40 +12,58 @@
                                 (= (:setting state) (:setting %))) setting-cost))))
 
 
-(defn find-states [need setting transition]
+(defn find-states [need setting academic-year transition]
   "Takes :need and :setting key and returns map containing values from transition data"
   (if (= :NONSEND (need transition))
-    [:NONSEND]
-    {:need (need transition ) :setting (setting transition)}))
+    :NONSEND
+    {:need (need transition) :setting (setting transition) :academic-year (academic-year transition)}))
 
 
-(defn set-of-input-states [transitions]
-  "Takes row maps from transitions.csv and returns set containing maps of possible states"
-  (let [state-1s (map #(find-states :need-1 :setting-1 %) transitions)
-        state-2s (map #(find-states :need-2 :setting-2 %) transitions)]
-    (-> (concat state-1s state-2s)
-        (set)
-        (disj [:NONSEND]))))
+(defn set-of-input-states [transitions with-academic-year?]
+  "Takes transitions and returns set containing maps of possible states"
+  (let [state-1s (map #(find-states :need-1 :setting-1 :academic-year-1 %) transitions)
+        state-2s (map #(find-states :need-2 :setting-2 :academic-year-2 %) transitions)
+        all-states (filter #(not= :NONSEND %) (concat state-1s state-2s))]
+    (if with-academic-year?
+      (set all-states)
+      (set (map #(dissoc % :academic-year) all-states)))))
 
 
 (defn check-missing-costs [transitions setting-cost]
-  (let [states (set-of-input-states transitions)]
+  "Produces warnings for states without costs via REPL and SEND_report.md"
+  (let [states (set-of-input-states transitions false)]
     (doseq [s states]
       (if-not (cost-for-state? s setting-cost)
-        (do (report/info (report/bold "Inconsistent inputs!")
+        (do (r/info (r/bold "Inconsistent inputs!")
                          " Missing cost for state in transitions.csv: "
-                         (str (:need s)) (str (:setting s)) "\n")
-            (repl-warn "Entry in transitions.csv without cost: "(:need s) (:setting s)))))))
+                         (str (:need s) (:setting s)) "\n")
+            (repl-warn "Entry in transitions without cost: "(:need s) (:setting s)))))))
 
 
+(defn valid-ay-for-state? [state valid-ays]
+  "Takes map containing state with academic year and returns true for valid states"
+  (some? (some true? (map #(and (= (:setting state) (:setting %))
+                                (and (>= (:academic-year state) (:min-academic-year %))
+                                     (<= (:academic-year state) (:max-academic-year %))))
+                          valid-ays))))
 
-;
 
-; every state has a cost
+(defn check-states-in-valid-ays [transitions valid-setting-academic-years]
+  "Produces warnings for states that are outside valid academic years via REPL and SEND_report.md"
+  (let [states (set-of-input-states transitions true)]
+    (doseq [s states]
+      (if-not (valid-ay-for-state? s valid-setting-academic-years)
+        (do (r/info (r/bold "Inconsistent inputs!")
+                    " Invalid setting for academic year in transitions.csv: "
+                    (str (:need s) (:setting s) " academic-year:" (:academic-year s)))
+            (repl-warn "Invalid academic year in transitions: "  (str (:need s) (:setting s) " academic-year:" (:academic-year s))))))))
 
-; check all input in transitions are in valid states
 
-; check population complete for each input year
+(defn run-input-checks [transitions setting-cost valid-setting-academic-years]
+  "Takes row-maps of input CSVs and runs checks"
+  (check-missing-costs transitions setting-cost)
+  (check-states-in-valid-ays transitions valid-setting-academic-years))
+
 
 
 
---
+++ test/witan/send/check_inputs_test.clj
@@ -0,0 +1,20 @@
+(ns witan.send.check-inputs-test
+  (:require [clojure.test :refer :all]
+            [witan.send.check-inputs :refer :all]))
+
+
+(def test-setting-costs [{:need :X, :setting :Y, :cost 999.9}])
+(def test-valid-setting-ay [{:setting :X, :min-academic-year -4 :max-academic-year 20}])
+
+
+(deftest cost-for-state?-tests
+  (is (= true (cost-for-state? {:need :X, :setting :Y} test-setting-costs)))
+  (is (= false (cost-for-state? {:need :X, :setting :Z} test-setting-costs))))
+
+
+(deftest valid-ay-for-state?-tests
+  (is (= false (valid-ay-for-state? {:setting :X :academic-year -5} test-valid-setting-ay)))
+  (is (= true (valid-ay-for-state? {:setting :X :academic-year -4} test-valid-setting-ay)))
+  (is (= true (valid-ay-for-state? {:setting :X :academic-year 10} test-valid-setting-ay)))
+  (is (= true (valid-ay-for-state? {:setting :X :academic-year 20} test-valid-setting-ay)))
+  (is (= false (valid-ay-for-state? {:setting :X :academic-year 21} test-valid-setting-ay))))
