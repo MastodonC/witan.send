8754a08bd37097ce24bba8fbac090b0283269dc0
Matt Ford <matt@dancingfrog.co.uk>
2018-07-18 13:01:11 +0100

    Move to project based dir structure

--- src/clj/witan/send/main.clj
+++ src/clj/witan/send/main.clj
@@ -6,14 +6,16 @@
             [aero.core :refer [read-config]])
   (:gen-class))
 
-(defn config [config-file]
+(defn config [project-dir]
   "Read a config file and merge it with schema inputs"
-  (merge (read-config config-file)
+  (merge (read-config (str project-dir "/config.edn"))
          {:schema-inputs {:settings-to-change sc/SettingsToChange
                           :transition-matrix sc/TransitionCounts
                           :population sc/PopulationDataset
                           :setting-cost sc/NeedSettingCost
-                          :valid-setting-academic-years sc/ValidSettingAcademicYears}}))
+                          :valid-setting-academic-years sc/ValidSettingAcademicYears}}
+         {:project-dir project-dir}
+         {:output-parameters {:project-dir project-dir}}))
 
 (defn run-send
   "Run the send model, the function expects a map as seen in
@@ -21,13 +23,13 @@
   generate it)"
   [config]
   (generate-report-header config)  
-  (-> (send/build-input-datasets (:file-inputs config) (:schema-inputs config))
+  (-> (send/build-input-datasets (:project-dir config) (:file-inputs config) (:schema-inputs config))
       (send/prepare-send-inputs (:transition-parameters config))
       (send/run-send-model (:run-parameters config))
       (send/output-send-results (:output-parameters config))))
 
 (defn -main
-  "Run the send model, defaulting to the demo if no config file passed in."
-  ([] (-main "data/demo/config.edn"))
-  ([config-file]
-   (run-send (config config-file))))
+  "Run the send model, defaulting to the inbuilt demo data if no project passed in."
+  ([] (-main "data/demo/"))
+  ([project-dir]
+   (run-send (config project-dir))))
