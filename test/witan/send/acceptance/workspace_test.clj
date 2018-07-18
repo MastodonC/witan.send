d0e8d9c2a92789a3674eee0c4fc1a5eb8cd6cfaa
Matt Ford <matt@dancingfrog.co.uk>
2018-07-18 13:01:11 +0100

    Move to project based dir structure

--- test/witan/send/acceptance/workspace_test.clj
+++ test/witan/send/acceptance/workspace_test.clj
@@ -5,7 +5,7 @@
 (deftest send-test
   (is (= #{:total-in-send-by-ay :total-in-send-by-ay-group 
            :total-in-send :total-in-send-by-need :total-in-send-by-setting}
-         (-> (run-send (config "demo/data/config.edn"))
+         (-> (run-send (config "demo/data"))
              (first)
              (keys)
              (set)))))
