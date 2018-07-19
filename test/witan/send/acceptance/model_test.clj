38ee2481f4eea44c1ff6eb36604031175020721b
Matt Ford <matt@dancingfrog.co.uk>
2018-07-19 18:51:31 +0100

    Rename test and fix inputs

--- test/witan/send/acceptance/model_test.clj
+++ test/witan/send/acceptance/model_test.clj
@@ -1,11 +1,11 @@
-(ns witan.send.acceptance.workspace-test
+(ns witan.send.acceptance.model-test
   (:require [clojure.test :refer :all]
             [witan.send.main :refer [config run-send]]))
 
 (deftest send-test
-  (is (= #{:total-in-send-by-ay :total-in-send-by-ay-group 
+  (is (= #{:total-in-send-by-ay :total-in-send-by-ay-group :by-state :total-cost
            :total-in-send :total-in-send-by-need :total-in-send-by-setting}
-         (-> (run-send (config "demo/data"))
+         (-> (run-send (config "data/demo"))
              (first)
              (keys)
              (set)))))
