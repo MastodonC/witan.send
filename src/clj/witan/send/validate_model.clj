8663c2f1d3bc79eaa9b032488c2dcb0a2569d4ca
mikeyford <mfogeo@gmail.com>
2018-07-20 15:57:49 +0100

    fix validation

--- src/clj/witan/send/validate_model.clj
+++ src/clj/witan/send/validate_model.clj
@@ -3,17 +3,16 @@
             [clojure.java.io :as io]
             [clojure.string :as str]
             [me.raynes.fs :refer [copy-dir]]
-            [witan.send.acceptance.workspace-test :refer [run-model]]))
+            [witan.send.main :refer [run-send config]]))
 
 
-(def static-input-files ["modify-settings.csv" "population.csv" "need-setting-costs.csv" "valid-setting-academic-years.csv"])
-
-
-(defn temp-dir [input-path]
-  (str "data/" input-path "temp/"))
+(defn temp-dir [project-dir]
+  "Returns path to validation temp dir for project"
+  (str project-dir "data/temp/"))
 
 
 (defn csv-data->maps [csv-data]
+  "Returns CSV data as seq of maps"
   (map zipmap
        (->> (first csv-data)
             (map keyword)
@@ -22,14 +21,16 @@
 
 
 (defn write-csv [file data]
+  "Write seq of maps to CSV"
   (with-open [writer (io/writer (io/file file))]
     (let [columns (into [] (keys (first data)))
           headers (mapv name columns)
           rows (mapv #(mapv % columns) data)]
-    (csv/write-csv writer (into [headers] rows)))))
+      (csv/write-csv writer (into [headers] rows)))))
 
 
 (defn load-csv-as-maps [file]
+  "Loads CSV data as maps"
   (csv-data->maps
     (with-open [reader (io/reader file)]
       (doall
@@ -37,6 +38,7 @@
 
 
 (defn get-validation-years [transitions]
+  "Returns list of years to use for delineate validation folds"
   (->> (map :calendar-year transitions)
        (distinct)
        (map #(Integer/parseInt %))
@@ -44,34 +46,18 @@
 
 
 (defn return-fold [bound-fn year transitions]
+  "Splits transitions data based on condition passed with bound-fn and year e.g. > 2016"
   (filter #(bound-fn (Integer/parseInt (:calendar-year %)) year) transitions))
 
 
-(defn copy-if-exists [source-path destination-path file]
-  (let [source (str source-path file)]
-    (if (.exists (clojure.java.io/as-file source))
-      (io/copy (io/file source) (io/file (str destination-path file))))))
-
-
-(defn copy-input-files [input-path year]
-  (let [source-path (str "data/" input-path)
-        destination-path (str (temp-dir input-path) year "/")]
-    (.mkdir (java.io.File. destination-path))
-    (doseq [file static-input-files]
-      (copy-if-exists source-path destination-path file))))
-
-
-(defn move-target-files [input-path year]
-  (doseq [file ["Output_Count.csv" "Output_AY_State.csv"]]
-    (io/copy (io/file (str "target/" file)) (io/file (str (temp-dir input-path) year "_" file)))))
-
-
 (defn return-testable-data [file test-years]
+  "Creates subset of model output for years where there is ground truth for comparison"
   (->> (for [year test-years] (filter #(= year (:calendar-year %)) (load-csv-as-maps file)))
        (mapcat seq)))
 
 
 (defn append-count-with-test [model-count test-data n-transitions]
+  "Adds total population count ground truth to model output"
   (let [test-year (str (dec (Integer/parseInt (:calendar-year model-count))))
         test-data-for-year  (filter #(= test-year (:calendar-year %)) test-data)
         send-test-data-for-year  (filter #(not= "NONSEND" (:need-2 %)) test-data-for-year)
@@ -80,6 +66,7 @@
 
 
 (defn append-state-with-test [model-state test-data n-transitions]
+  "Adds states ground truth from test-data to model output"
   (let [test-year (str (dec (Integer/parseInt (:calendar-year model-state))))
         [need setting] (str/split (str/replace (:state model-state) ":" "") #"-")
         academic-year (:academic-year model-state)
@@ -91,52 +78,80 @@
     (assoc model-state :ground-truth (str ground-truth-count) :n-transitions (str n-transitions))))
 
 
-(defn collate-fold [input-path year n-transitions]
-  (let [test-data (load-csv-as-maps (str (temp-dir input-path) "test_" year ".csv"))
-        test-years (->> (map :calendar-year test-data)
+(defn collate-fold [project-dir test-data year n-transitions]
+  (let [test-years (->> (map :calendar-year test-data)
                         (distinct)
                         (map #(inc (Integer/parseInt %)))
                         (map str))
-        model-state (return-testable-data (str (temp-dir input-path) year "_Output_AY_State.csv") test-years)
-        model-count (return-testable-data (str (temp-dir input-path) year "_Output_Count.csv") test-years)
+        model-state (return-testable-data (str (temp-dir project-dir) year "/Output_AY_State.csv") test-years)
+        model-count (return-testable-data (str (temp-dir project-dir) year "/Output_Count.csv") test-years)
         count-results (map #(append-count-with-test % test-data n-transitions) model-count)
         state-results (map #(append-state-with-test % test-data n-transitions) model-state)]
-    (write-csv (str (temp-dir input-path) "results_" year "_count.csv") count-results)
-    (write-csv (str (temp-dir input-path) "results_" year "_state.csv") state-results)))
+    {:count count-results :state state-results}))
 
 
-(defn validate-fold [input-path year transitions settings]
-  (let [train-data (return-fold <= year transitions)
+(defn validate-fold [config year]
+  "Splits project transitions.csv into training and test data, before and after year argument.
+  Runs model with config, overriding default transition matrix with training data.
+  Returns map containing results of comparing model predictions with test data for total count and individual states"
+
+  (let [project-dir (:project-dir config)
+        transitions (load-csv-as-maps (str project-dir (:transition-matrix (:file-inputs config))))
+        train-data (return-fold <= year transitions)
         n-transitions (count (distinct (map :calendar-year train-data)))
         test-data (return-fold > year transitions)
-        fold-settings (merge settings {:output? true :override-inputs-path (str input-path "temp/" year "/")})]
-    (copy-input-files input-path year)
-    (write-csv (str (temp-dir input-path) year "/transitions.csv") train-data)
-    (write-csv (str (temp-dir input-path) "test_" year ".csv") test-data)
-    (run-model fold-settings)
-    (move-target-files input-path year)
-    (collate-fold input-path year n-transitions)))
+        fold-train-path (str "data/temp/" year "/transitions.csv")
+        fold-config (-> config
+                      (assoc-in [:file-inputs :transition-matrix] fold-train-path)
+                      (assoc-in [:run-parameters :seed-year] (inc year))
+                      (assoc-in [:output-parameters :run-charts] false)
+                      (assoc-in [:output-parameters :output-dir] (str "data/temp/" year "/")))]
+    (.mkdir (java.io.File. (str project-dir "data/temp/" year "/")))
+    (write-csv (str project-dir fold-train-path) train-data)
+    (write-csv (str (temp-dir project-dir) year "/test.csv") test-data)
+    (run-send fold-config)
+    (collate-fold project-dir test-data year n-transitions)))
 
 
-(defn run-validation [input-path settings-map keep-temp-files?]
-  "Function to validate SEND model.
-  input-path is a string with the data directory to validate the model on e.g. demo/
-  settings-map expects a hash map containing run-model parameters to use for validation eg. {:iterations 100}
-  (note that run-model's :output? and :override-inputs-path arguments are both required for validation and are overwritten if provided)
-  keep-temp-files? can be set to false unless debugging etc."
-  (doseq [path [(temp-dir input-path) "validation/"]]
-    (.mkdir (java.io.File. path)))
-  (let [transitions (load-csv-as-maps (str "data/" input-path "transitions.csv"))
-        years-to-validate (get-validation-years transitions)]
-    (doseq [year years-to-validate]
-      (validate-fold input-path year transitions settings-map))
-    (->> (map #(load-csv-as-maps (str "data/" input-path "temp/results_" % "_count.csv")) years-to-validate)
-         (flatten)
-         (write-csv (str "validation/validation_result_count_" (str/join (drop-last input-path)) ".csv")))
-    (->> (map #(load-csv-as-maps (str "data/" input-path "temp/results_" % "_state.csv")) years-to-validate)
-         (flatten)
-         (write-csv (str "validation/validation_result_state_" (str/join (drop-last input-path)) ".csv"))))
+(defn write-validation-results [project-dir results]
+  "Takes seq of maps containing results returned by (validate-fold) and writes to disk"
+  (->> (map :count results)
+       flatten
+       (write-csv (str project-dir "validation/validation_result_count.csv")))
+  (->> (map :state results)
+       flatten
+       (write-csv (str project-dir "validation/validation_results_state.csv"))))
+
+
+(defn setup-validation-dirs [project-dir]
+  "Create dirs required for validation process"
+  (doseq [dir [(temp-dir project-dir) (str project-dir "validation/")]]
+    (.mkdir (java.io.File. dir))))
+
+
+(defn tear-down-validation-dirs [project-dir keep-temp-files?]
+  "Remove input data and results for separate folds, move to validation dir if keep-temp-files is true"
   (if keep-temp-files?
-    (copy-dir (temp-dir input-path) (str "validation/" (str/join (drop-last input-path)) "_temp_files/")))
-  (doseq [file (reverse (file-seq (io/file (temp-dir input-path))))]
+    (copy-dir (temp-dir project-dir) (str project-dir "validation/")))
+  (doseq [file (reverse (file-seq (io/file (temp-dir project-dir))))]
     (io/delete-file file)))
+
+
+(defn run-validation [project-dir keep-temp-files?]
+  "Main function to validate SEND model.
+
+  Args:
+    project-dir: the same path you would would pass to run the main send model, which must contain a config.edn file
+    keep-temp-files? true if you wish to inspect training, test and output data for individual folds, false otherwise
+
+  Results are stored in a directory called validation within the project"
+
+  (setup-validation-dirs project-dir)
+  (let [config (config project-dir)
+        years-to-validate (-> (str project-dir (:transition-matrix (:file-inputs config)))
+                              load-csv-as-maps
+                              get-validation-years)
+        results (doall (map #(validate-fold config %) years-to-validate))]
+    (write-validation-results project-dir results))
+  (tear-down-validation-dirs project-dir keep-temp-files?))
+
