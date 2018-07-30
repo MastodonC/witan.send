65e05199dbe37146e77d4a36cfa0331a8f53186a
Matt Ford <matt@dancingfrog.co.uk>
2018-07-30 09:50:24 +0100

    Delete the expected outputs before a run.

--- test/witan/send/acceptance/model_test.clj
+++ test/witan/send/acceptance/model_test.clj
@@ -2,22 +2,24 @@
   (:require [clojure.test :refer :all]
             [witan.send.main :as m]
             [witan.send.send :as send]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.string :refer [join]]))
 
 (deftest expected-results
-  (let [expected-md5s {"historic-data.csv" "208d196285b0648f6f66697b32dcbc4e"
-                       "Output_AY.csv" "87a50609a022e34478d17deed082ad7f"
+  (let [expected-md5s {"Output_AY.csv" "87a50609a022e34478d17deed082ad7f"
                        "Output_AY_Group.csv" "51e18eafb686ce388c25a46a23a0d29a"
                        "Output_AY_State.csv" "70fd124bb0f7b5093e800bc63ce1c991"
                        "Output_Cost.csv" "1d7556cc3189491bf824a9785bddfb2c"
                        "Output_Count.csv" "1f84ff94394046f8d37da8bfa4834a0b"
                        "Output_Need.csv" "023b0a445d338f55dcfeee3a3a3677c6"
                        "Output_Setting.csv" "4f0d1e51a49f678df5637c33c604bbe1"
-                       "transitions.edn" "4ea4b6076e4fed0ce36cfeeece5aa901"
-                       "valid-settings.csv" "7035222149956f42fb5128126296817b"}
+                       "transitions.edn" "4ea4b6076e4fed0ce36cfeeece5aa901"}
         files (keys expected-md5s)
         config (m/config "data/demo")
         output-dir (m/get-output-dir config)]
+    (run! #(let [file (join "/" [output-dir %])]
+             (when (.exists (io/file file))
+               (io/delete-file file))) files)
     (->(m/run-send config)
        (send/output-send-results (:output-parameters config)))
     (is (= expected-md5s
