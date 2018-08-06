5034892bc034df1fdd6d72c28c7faa2369043c83
Matt Ford <matt@dancingfrog.co.uk>
2018-08-06 14:06:27 +0100

    Add validation to config file
    Also
    
    - make main only deal with config parsing and selecting what to run
    - make validate use the top-level config parsing (removes duplicate code)

--- test/witan/send/acceptance/validate_model_test.clj
+++ test/witan/send/acceptance/validate_model_test.clj
@@ -2,18 +2,19 @@
   (:require [clojure.test :refer :all]
             [clojure.java.io :as io]
             [clojure.string :refer [join]]
-            [witan.send.validate-model :as v]))
+            [witan.send.main :as m]
+            [witan.send.validate-model :as vm]))
 
 (deftest expected-validation-results
   (let [expected-md5s {"validation_results_count.csv" "cf6265c280c5d7d125cc6924bc89f2d8"
                        "validation_results_state.csv" "71c3dc451f8e3e42a75128f78572b57e"}
         files (keys expected-md5s)
-        config-path "data/demo/config.edn"
-        validation-dir (io/file (.getParent (java.io.File. config-path)) "validation")]
+        config (m/config "data/demo/config.edn")
+        validation-dir (join "/" [(:project-dir config) "validation"])]
     (run! #(let [file (join "/" [validation-dir %])]
              (when (.exists (io/file file))
                (io/delete-file file))) files)
-    (v/run-validation config-path)
+    (vm/run-send-validation config)
     (is (= expected-md5s
            (into {} (for [f files]
                       [f (-> (io/file validation-dir f) (digest/md5))]))))))
