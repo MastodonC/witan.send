d77e7290b596a71a8e5a8354fec657b776ba2c85
Bruce Durling <bruce@mastodonc.com>
2018-10-19 16:45:38 +0100

    Upgraded clojure to 1.9

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/mastodonc/witan.send"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.hdrhistogram/HdrHistogram "2.1.9"]
                  [net.mikera/core.matrix "0.55.0" :exclusions [org.clojure/clojure]]
                  [org.clojure/test.check "0.9.0"]
