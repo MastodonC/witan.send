0dc1ff693c8bd8e8e0bbeac4b7efdccd5cfd8287
Seb Bailey <sebastian@mastodonc.com>
2018-10-22 15:38:04 +0100

    Test to check all plots are produced

--- test/witan/send/acceptance/model_test.clj
+++ test/witan/send/acceptance/model_test.clj
@@ -4,7 +4,8 @@
             [witan.send.model.output :as so]
             [witan.send.main :as m]
             [clojure.java.io :as io]
-            [clojure.string :refer [join]]))
+            [clojure.string :refer [join]]
+            [witan.send.validate-model :as v]))
 
 (deftest expected-results
   (let [expected-md5s {"Output_AY.csv" "9e94286fd0bd9c5f11f6a05fdc059a22",
@@ -15,7 +16,15 @@
                        "Output_Need.csv" "f51084834e693186668087d26278b322",
                        "Output_Setting.csv" "4e0665ddab7c45e966ca9df2d089f42c",
                        "transitions.edn" "27ca57ba18f6024d335fd57722ef1cbf"}
-        config (m/config "data/demo/config.edn")
+        historic-years (distinct (map :calendar-year (v/load-csv-as-maps "data/demo/data/transitions.csv")))
+        expected-plots (into ["Joiner_Probability.pdf" "Joiner_Transitions.pdf" "Leaver_Probability.pdf"
+                              "Mover_Probability.pdf" "NCY_Population_Trends.pdf" "Need_Trends.pdf"
+                              "Setting_Type_Counts.pdf" "Settings_Trends_1.pdf" "Settings_Trends_2.pdf"
+                              "Settings_Trends_3.pdf" "Settings_Trends_4.pdf"
+                              "Special_Setting_Counts.pdf" "Total_Cost.pdf" "Total_Population.pdf"]
+                             (map #(str "Historic_Transitions_" % ".pdf") historic-years))
+        files (keys expected-md5s)
+        config (assoc-in (m/config "data/demo/config.edn") [:output-parameters :run-charts] true)
         output-dir (m/get-output-dir config)]
     (run! #(let [file (join "/" [output-dir %])]
              (when (.exists (io/file file))
@@ -25,4 +34,6 @@
     (testing "checksums are unchanged"
       (is (= expected-md5s
              (into {} (for [f files]
-                        [f (-> (io/file output-dir f) (digest/md5))])))))))
+                        [f (-> (io/file output-dir f) (digest/md5))])))))
+    (testing "all plots are produced"
+      (is (every? true? (map #(.exists (io/file (join "/" [output-dir %]))) expected-plots))))))
