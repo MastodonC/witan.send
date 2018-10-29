b97828fc399c855cbc826abc165c0a3ce89c9cbd
Seb Bailey <sebastian@mastodonc.com>
2018-10-29 14:01:52 +0000

    Confidence interval test

--- test/witan/send/utils_test.clj
+++ test/witan/send/utils_test.clj
@@ -9,3 +9,8 @@
 (deftest load-csv-test
   (testing "Empty lines in input csv are removed"
     (is (= (load-csv "data/demo/data/test.csv") {:column-names ["need" "setting" "cost"], :columns [["CI" "EO" "4577.3"]]}))))
+
+(deftest confidence-intervals-test
+  (testing "95% confidence intervals correctly calculated as per https://www.dummies.com/education/math/statistics/how-to-calculate-a-confidence-interval-for-a-population-mean-when-you-know-its-standard-deviation/"
+    (is (= 7.0492 (:low-ci (confidence-intervals {:mean 7.5 :std-dev 2.3} 100))))
+    (is (= 7.9508 (:high-ci (confidence-intervals {:mean 7.5 :std-dev 2.3} 100))))))
