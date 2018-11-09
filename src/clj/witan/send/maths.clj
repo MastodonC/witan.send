4e52d0b2a4bda200c37e0de80a195f7cb36a2fd1
Bruce Durling <bruce@mastodonc.com>
2018-11-09 10:06:49 +0000

    Added docstring as per @seb231

--- src/clj/witan/send/maths.clj
+++ src/clj/witan/send/maths.clj
@@ -4,5 +4,7 @@
   "x + y. Returns y if x is nil and x if y is nil."
   (fnil + 0))
 
-(defn round [x]
+(defn round
+  "This rounds numbers to 2 decimal places."
+  [x]
   (Double/parseDouble (format "%.02f" (double x))))
