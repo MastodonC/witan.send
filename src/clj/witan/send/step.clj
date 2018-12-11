730af38963a294d810a00d2d120102089f8d36f4
Matt Ford <matt@dancingfrog.co.uk>
2018-12-11 17:09:50 +0000

    Remove unnecessary external population injection into Year state
    https://gist.github.com/mattford63/03031a1431b0b83146cf31598489b584#file-send-step-mechanism-org

--- src/clj/witan/send/step.clj
+++ src/clj/witan/send/step.clj
@@ -3,10 +3,9 @@
 
 (defn age-population
   [projection model-state]
-  (-> (reduce (fn [coll [[year state] population]]
-                (cond-> coll
-                  (< year c/max-academic-year)
-                  (assoc [(inc year) state] population)))
-              {}
-              model-state)
-      (assoc [c/min-academic-year c/non-send] (get projection c/min-academic-year))))
+  (reduce (fn [coll [[year state] population]]
+            (cond-> coll
+              (< year c/max-academic-year)
+              (assoc [(inc year) state] population)))
+          {}
+          model-state))
