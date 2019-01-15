24e998bd77f6d988c4f0a684c913f6acf86eac8e
Seb Bailey <sebastian@mastodonc.com>
2019-01-15 12:20:10 +0000

    Scenario projection md5 test

--- test/witan/send/acceptance/model_test.clj
+++ test/witan/send/acceptance/model_test.clj
@@ -40,3 +40,25 @@
     ;;; Following test works locally, but requires R install on CircleCI
     #_(testing "all plots are produced"
         (is (every? true? (map #(.exists (io/file (join "/" [output-dir %]))) expected-plots))))))
+
+(deftest expected-filter-by-ay11-cy2015-results
+  (let [expected-md5s {"Output_AY.csv" "89022266998810dd7f4c1c8283c16744",
+                       "Output_AY_Group.csv" "e834a4f17eae2da61a542aa132dedd47",
+                       "Output_State.csv" "b15b768bd3a53bfe74f0170fd9a3d481",
+                       "Output_Cost.csv" "c672ba67c5f6c004dca67ace5fd4d4ab",
+                       "Output_Count.csv" "6c8443f1e724ebda02e3523bca7bc1d5",
+                       "Output_Need.csv" "854c72e0cfe17d9a7463b6fb5163cf13",
+                       "Output_Setting.csv" "dee6241d19a76bb1b6a9058d8e4e70ce",
+                       "transitions.edn" "0144496f21ad6b7c44243acfc4bf98f3"}
+        files (keys expected-md5s)
+        config (m/read-config "data/demo/config_splicing.edn")
+        output-dir (m/get-output-dir config)]
+    (run! #(let [file (join "/" [output-dir %])]
+             (when (.exists (io/file file))
+               (io/delete-file file))) files)
+    (-> (send/run-send-workflow config)
+        (so/output-send-results (:output-parameters config)))
+    (testing "checksums are unchanged for scenario"
+      (is (= expected-md5s
+             (into {} (for [f files]
+                        [f (-> (io/file output-dir f) (digest/md5))])))))))
