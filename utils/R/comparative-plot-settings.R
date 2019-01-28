941645b65105827a60da0ff25efd9f5a849283ee
Seb Bailey <sebastian@mastodonc.com>
2019-01-28 09:30:39 +0000

    Standardise comparison plots

--- utils/R/comparative-plot-settings.R
+++ utils/R/comparative-plot-settings.R
@@ -18,8 +18,8 @@ comparative_plot_settings = function(output, data1, data2){
   max_x = max(count_data1$calendar.year)
   
   ## data labels for plots
-  data1_label = str_to_title(sapply(strsplit(data1, "/"), tail, 1)) # this doesn't appear to work
-  data2_label = str_to_title(sapply(strsplit(data2, "/"), tail, 1))
+  #data1_label = str_to_title(sapply(strsplit(data1, "/"), tail, 1)) # this doesn't appear to work
+  #data2_label = str_to_title(sapply(strsplit(data2, "/"), tail, 1))
 
   ## ribbon plot version - not zero-indexed
   for(i in settings) {
@@ -37,23 +37,24 @@ comparative_plot_settings = function(output, data1, data2){
       labs(y = paste(i, "SEND Population")) +
       scale_x_continuous(name="Year",
                          breaks = seq(min_x, max_x),
-                         limits = c(min_x, max_x+1.5)) +
+                         limits = c(min_x, max_x)) +
       ggtitle(paste(i, "Population Projection")) +
       theme(axis.text.x = element_text(size=8)) +
-      geom_text(x=max_x+1,
-                y=df1$mean[df1$calendar.year==max_x]+20,
-                label= data1_label,
-                colour = cols[1]) +
-      geom_text(x=max_x+1,
-                y=df2$mean[df2$calendar.year==max_x]-20,
-                label= data2_label,
-                colour = cols[2]) +
-      theme(legend.position="none")
+      # geom_text(x=max_x+1,
+      #           y=df1$mean[df1$calendar.year==max_x]+20,
+      #           label= data1_label,
+      #           colour = cols[1]) +
+      # geom_text(x=max_x+1,
+      #           y=df2$mean[df2$calendar.year==max_x]-20,
+      #           label= data2_label,
+      #           colour = cols[2]) +
+      theme(legend.position="none") +
+      theme_bw()
     
     ggsave(paste0(output, "comparisons/", i, "_Population_Comparative.png"),
            width=8,
            height=6,
-           units="in",
+           dpi = 400,
            device = "png")
     }
   
