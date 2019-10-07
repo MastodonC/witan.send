51c8086d3a7bbad4e3007fe07eee561279c92f04
Seb Bailey <sebastian@mastodonc.com>
2019-10-07 11:37:28 +0100

    Break get-target-pop into two fns for interactive investigation

--- src/clj/witan/send/goal_seek.clj
+++ src/clj/witan/send/goal_seek.clj
@@ -21,7 +21,9 @@
                       (update :population i/->int)))
             filepath))
 
-(defn get-target-pop [config]
+(defn state-pop [config]
+  "Takes a config and returns the state to be modified and output path of
+   Output_State_pop_only.csv results"
   (let [state (-> (get-in config [:transition-parameters :transitions-to-change])
                   first
                   (clojure.set/rename-keys {:setting-2 :setting :need-2 :need :academic-year-2 :ay})
@@ -30,13 +32,17 @@
         state-pop (str (:project-dir config)
                        "/"
                        out-dir
-                       "/Output_State_pop_only.csv")
-        results (filter #(and (= (:setting state) (:setting %))
-                              (= (:need state) (:need %)))
-                        (csv->state-pop state-pop))]
-    (map #(merge (assoc {} :year (key %))
-                 (apply merge-with + (map (fn [m] (select-keys m [:population])) (val %))))
-         (group-by :calendar-year results))))
+                       "/Output_State_pop_only.csv")]
+    [state state-pop]))
+
+(defn get-target-pop [[state state-pop]]
+  "Takes a vector with a state to find (e.g. {:setting :SFSS, :need :ASD}) and filepath
+  for a Output_State_pop_only.csv file to return summed population over calender years"
+  (map #(merge (assoc {} :year (key %))
+               (apply merge-with + (map (fn [m] (select-keys m [:population])) (val %))))
+       (group-by :calendar-year (filter #(and (= (:setting state) (:setting %))
+                                              (= (:need state) (:need %)))
+                                        (csv->state-pop state-pop)))))
 
 (defn update-results-path [config]
   (assoc-in config [:output-parameters :output-dir] (str "results/" (get-in config [:output-parameters :output-dir]))))
@@ -63,7 +69,7 @@
   (loop [configs (map update-results-path (mc/generate-configs (create-transition-modifier-seq m start end step) base-config))]
     (let [[config & rest-configs] configs
           result (do (main/run-recorded-send config)
-                     (get-target-pop config))
+                     (get-target-pop (state-pop config)))
           current-pop (get-current-pop (:year target) result)
           diff (pop-diff-by-year (:year target) result)]
       (if (target-pop-exceeded? current-pop (:population target))
