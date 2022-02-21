6c30a83f14a9d9ff47c58e7a99e1955130f42788
Seb Bailey <sebastian@mastodonc.com>
2022-02-21 08:35:46 +0000

    Add merge-with back in

--- src/clj/witan/send/main.clj
+++ src/clj/witan/send/main.clj
@@ -23,10 +23,10 @@
   [config-path]
   (let [project-dir (or (.getParent (io/as-file config-path))
                         (System/getProperty "user.dir"))]
-    (merge (aero/read-config config-path)
-           default-schemas
-           {:project-dir project-dir}
-           {:output-parameters {:project-dir project-dir}})))
+    (merge-with merge (aero/read-config config-path)
+                default-schemas
+                {:project-dir project-dir}
+                {:output-parameters {:project-dir project-dir}})))
 
 (defn get-output-dir [config]
   (string/join "/" [(:project-dir config)
