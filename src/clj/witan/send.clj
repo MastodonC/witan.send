fcde3f900bd2abd5dcc25999b9dbe9ce628b2591
Bruce Durling <bruce@mastodonc.com>
2022-03-16 18:46:57 +0000

    Parquet output
    This should probably go into another library, but hopefully it is
    isolated enough that it is easy to move.

--- src/clj/witan/send.clj
+++ src/clj/witan/send.clj
@@ -1,9 +1,7 @@
 (ns witan.send
   (:require [aero.core :as aero]
             [clojure.java.io :as io]
-            [clojure.core.async :as async]
             [medley.core :as medley]
-            [tablecloth.api :as tc]
             [witan.send.distributions :as d]
             [witan.send.maths :as m]
             [witan.send.model.prepare :as p]
@@ -15,7 +13,7 @@
             [witan.send.params :as params]
             [witan.send.states :as states]))
 
-;; FIXME: this should go in its own config namespace
+;; FIXME: this should go into witan.send.config
 (defn read-config
   "Read a config file and merge it with schema inputs"
   [config-path]
@@ -27,10 +25,11 @@
                 {:project-dir project-dir}
                 {:output-parameters {:project-dir project-dir}})))
 
+;; FIXME: this should go into witan.send.input
 (defn build-input-datasets
   "Build a map of the datasets to use for input"
   [project-dir {:keys [_costs population settings-to-change transitions valid-states]}]
-  (let [input-datasets {;; :costs (wic/csv->costs (str project-dir "/" costs))
+  (let [input-datasets { ;; :costs (wic/csv->costs (str project-dir "/" costs))
                         :population (wip/csv->population (str project-dir "/" population))
                         :transitions (wit/csv->transitions (str project-dir "/" transitions))
                         :valid-states (wivs/csv->valid-states (str project-dir "/" valid-states))}]
@@ -54,7 +53,7 @@
           {}
           dataset))
 
-
+;; FIXME: this should go into witan.send.train
 (defn train-model
   "train-model returns a map that can populate `:standard-projection`
   or `:scenario-projection` when running the model.
@@ -106,119 +105,7 @@
 (defn seed-year [transitions]
   (inc (reduce max (map :calendar-year transitions))))
 
-#_(defn run-model [{:keys [model config]
-                    :as state}]
-    (r/run-send-model model (:projection-parameters config)))
-
-;; Deprecated
-;; (defn create-projections [simulations modify-transitions-date-range make-setting-invalid inputs modified-inputs population-by-state projected-population seed-year]
-;;   (map (fn [simulation-run]
-;;          [simulation-run (reductions (partial r/run-model-iteration
-;;                                               modify-transitions-date-range
-;;                                               make-setting-invalid
-;;                                               inputs
-;;                                               modified-inputs)
-;;                                      {:model population-by-state}
-;;                                      (r/projected-future-pop-by-year projected-population seed-year))])
-;;        (range simulations)))
-
-(defn create-projections-xf [modify-transitions-date-range make-setting-invalid inputs modified-inputs population-by-state projected-population seed-year]
-  (map (fn [simulation-run]
-         [simulation-run (reductions (partial r/run-model-iteration
-                                              modify-transitions-date-range
-                                              make-setting-invalid
-                                              inputs
-                                              modified-inputs)
-                                     {:model population-by-state}
-                                     (r/projected-future-pop-by-year projected-population seed-year))])))
-
-
-(defn unpack-transition-counts [{:keys [transitions simulation]}]
-  (map (fn [map-entry]
-         (let [key (first map-entry)
-               transition-count (second map-entry)
-               [need-1 setting-1] (-> key
-                                      (nth 2)
-                                      (states/split-need-setting))
-               [need-2 setting-2] (-> key
-                                      (nth 3)
-                                      (states/split-need-setting))]
-           {:calendar-year (dec (nth key 0))
-            :calendar-year-2 (nth key 0)
-            :academic-year-1 (dec (nth key 1))
-            :academic-year-2 (nth key 1)
-            :need-1 need-1
-            :setting-1 setting-1
-            :need-2 need-2
-            :setting-2 setting-2
-            :transition-count transition-count
-            :simulation simulation}))
-       transitions))
-
-(defn add-simulation-idx [simulation-idx sd]
-  (assoc sd :simulation simulation-idx))
-
-(def ->dataset-seq-xf
-  (comp
-   (map (fn [[simulation-idx simulation-data]]
-          (map (partial add-simulation-idx simulation-idx) simulation-data)))
-   (map (fn [simulation]
-          (as-> simulation $
-            (filter :transitions $)
-            (mapcat unpack-transition-counts $)
-            (tc/dataset $)
-            (tc/map-columns $ :need-1 [:need-1] name)
-            (tc/map-columns $ :need-2 [:need-2] name)
-            (tc/map-columns $ :setting-1 [:setting-1] name)
-            (tc/map-columns $ :setting-2 [:setting-2] name)
-            (tc/convert-types $ {:academic-year-1  :int16
-                                 :academic-year-2  :int16
-                                 :calendar-year    :int16
-                                 :calendar-year-2  :int16
-                                 :simulation       :int16
-                                 :transition-count :int32}))))))
-
-(defn ->dataset-seq [simulations]
-  (sequence
-   ->dataset-seq-xf
-   simulations))
-
-;; Deprecated
-;; (defn run-model
-;;   "Outputs a seq of datasets. One for each simulation.
-
-;;   Each simulation is made up of
-;;   :simulation
-;;   :calendar-year
-;;   :academic-year
-;;   :need-1 :setting-1
-;;   :need-2 :setting-2
-;;   :transition-count"
-;;   [{:keys [standard-projection scenario-projection modify-transition-by
-;;            modify-transitions-date-range seed-year make-setting-invalid]
-;;     :as _trained-model}
-;;    {:keys [random-seed simulations]
-;;     :as _projection-parameters}]
-;;   (d/set-seed! random-seed)
-;;   (println "Preparing" simulations "simulations...")
-;;   (let [{:keys [population population-by-state
-;;                 projected-population cost-lookup
-;;                 valid-states transitions] :as inputs} standard-projection
-;;         modified-inputs (when ((complement nil?) scenario-projection)
-;;                           (assoc scenario-projection :valid-year-settings
-;;                                  (states/calculate-valid-year-settings-from-setting-academic-years valid-states)))
-;;         inputs (assoc inputs :valid-year-settings (states/calculate-valid-year-settings-from-setting-academic-years valid-states))
-;;         projection (create-projections simulations
-;;                                        modify-transitions-date-range
-;;                                        make-setting-invalid
-;;                                        inputs
-;;                                        modified-inputs
-;;                                        population-by-state
-;;                                        projected-population
-;;                                        seed-year)]
-;;     (->dataset-seq projection)))
-
-(defn run-model-xf
+(defn create-projections-xf
   "Outputs a seq of datasets. One for each simulation.
 
   Each simulation is made up of
@@ -241,104 +128,11 @@
                                (assoc scenario-projection :valid-year-settings
                                       (states/calculate-valid-year-settings-from-setting-academic-years valid-states)))
         standard-projection' (assoc standard-projection :valid-year-settings (states/calculate-valid-year-settings-from-setting-academic-years valid-states))]
-    (create-projections-xf
-     modify-transitions-date-range
-     make-setting-invalid
-     standard-projection'
-     scenario-projection'
-     population-by-state
-     projected-population
-     seed-year)))
-
-
-(comment
-
-  ;; EXAMPLE: This is what a baseline run would look like
-  (def foo
-    (let [config (read-config "data/demo/config.edn")
-          input-datasets (build-input-datasets (:project-dir config) (:file-inputs config))
-          model (train-model {:input-datasets input-datasets
-                              :config config
-                              :print-warnings? true})
-          model-results (run-model model (:projection-parameters config))]
-      ;; mush the seq of datasets together (tho probably better to run
-      ;; it through a ds/reducer of some sort
-      ;; (apply tc/concat-copying model-results)
-      model-results))
-
-  (-> foo
-      first
-      (tc/info));; => _unnamed: descriptive-stats [10 12]:
-  ;;    |         :col-name | :datatype | :n-valid | :n-missing |   :min |         :mean |    :mode |   :max | :standard-deviation |       :skew |   :first |    :last |
-  ;;    |-------------------|-----------|---------:|-----------:|-------:|--------------:|----------|-------:|--------------------:|------------:|----------|----------|
-  ;;    |  :academic-year-1 |    :int16 |     4637 |          0 |   -3.0 |    7.34699159 |          |   19.0 |          4.90374763 | -0.12309428 |       14 |        2 |
-  ;;    |  :academic-year-2 |    :int16 |     4637 |          0 |   -2.0 |    8.34699159 |          |   20.0 |          4.90374763 | -0.12309428 |       15 |        3 |
-  ;;    |    :calendar-year |    :int16 |     4637 |          0 | 2017.0 | 2019.04356265 |          | 2021.0 |          1.41598178 | -0.03550839 |     2017 |     2021 |
-  ;;    |  :calendar-year-2 |    :int16 |     4637 |          0 | 2018.0 | 2020.04356265 |          | 2022.0 |          1.41598178 | -0.03550839 |     2018 |     2022 |
-  ;;    |           :need-1 |  :keyword |     4637 |          0 |        |               |       :T |        |                     |             |       :T |       :U |
-  ;;    |           :need-2 |  :keyword |     4637 |          0 |        |               | :NONSEND |        |                     |             | :NONSEND | :NONSEND |
-  ;;    |        :setting-1 |  :keyword |     4637 |          0 |        |               |       :B |        |                     |             |       :N |       :J |
-  ;;    |        :setting-2 |  :keyword |     4637 |          0 |        |               | :NONSEND |        |                     |             | :NONSEND | :NONSEND |
-  ;;    |       :simulation |    :int16 |     4637 |          0 |    0.0 |    0.00000000 |          |    0.0 |          0.00000000 |         NaN |        0 |        0 |
-  ;;    | :transition-count |    :int32 |     4637 |          0 |    0.0 |    2.77377615 |          |   79.0 |          7.21378957 |  6.33194149 |        0 |        0 |
-
-  ;; Or do each step so you can tweak the results of one
-  (def config (read-config "data/demo/config.edn"))
-  (def input-datasets (build-input-datasets (:project-dir config) (:file-inputs config)))
-  (def model (train-model {:input-datasets input-datasets
-                           :config config
-                           :print-warnings? true}))
-  (def model-results (run-model model (:projection-parameters config)))
-  (->dataset-seq model-results)
-
-  )
-
-
-(comment
-
-  ;; FIXME: I don't like these versions as much
-
-  ;; FIXME: I prefer ->dataset-seq-xf
-  (def ->dataset-xf
-    (comp
-     (mapcat (fn [[simulation-idx simulation-data]]
-               (map (fn [sd]
-                      (assoc sd :simulation simulation-idx))
-                    simulation-data)))
-     (filter :transitions)
-     (mapcat (fn [{:keys [transitions simulation]}]
-               (map (fn [map-entry]
-                      (let [key (first map-entry)
-                            transition-count (second map-entry)
-                            [need-1 setting-1] (-> key
-                                                   (nth 2)
-                                                   (states/split-need-setting))
-                            [need-2 setting-2] (-> key
-                                                   (nth 3)
-                                                   (states/split-need-setting))]
-                        {:calendar-year (nth key 0)
-                         :academic-year (nth key 1)
-                         :need-1 need-1
-                         :setting-1 setting-1
-                         :need-2 need-2
-                         :setting-2 setting-2
-                         :transition-count transition-count
-                         :simulation simulation}))
-                    transitions)))))
-
-  ;; FIXME: I prefer ->dataset-seq
-  (defn ->dataset [simulations]
-    (-> (let [to (async/chan 1024)]
-          (async/pipeline
-           (* 3 (quot (.availableProcessors (Runtime/getRuntime)) 4))
-           to
-           ->dataset-xf
-           (async/to-chan!! simulations))
-          (async/<!! (async/into [] to)))
-        (tc/dataset)
-        (tc/convert-types {:academic-year :int16
-                           :calendar-year :int16
-                           :simulation :int16
-                           :transition-count :int32})))
-
-  )
+    (map (fn [simulation-run]
+           [simulation-run (reductions (partial r/run-model-iteration
+                                                modify-transitions-date-range
+                                                make-setting-invalid
+                                                standard-projection'
+                                                scenario-projection')
+                                       {:model population-by-state}
+                                       (r/projected-future-pop-by-year projected-population seed-year))]))))
---
+++ src/clj/witan/send/parquet.clj
@@ -0,0 +1,101 @@
+(ns witan.send.parquet
+  (:require [com.climate.claypoole :as cp]
+            [com.climate.claypoole.lazy :as lazy]
+            [tablecloth.api :as tc]
+            [tech.v3.libs.parquet :as parquet]
+            [witan.send :as send]
+            [witan.send.states :as states]))
+
+(defn unpack-transition-counts [{:keys [transitions simulation]}]
+  (map (fn [map-entry]
+         (let [key (first map-entry)
+               transition-count (second map-entry)
+               [need-1 setting-1] (-> key
+                                      (nth 2)
+                                      (states/split-need-setting))
+               [need-2 setting-2] (-> key
+                                      (nth 3)
+                                      (states/split-need-setting))]
+           {:calendar-year (dec (nth key 0))
+            :calendar-year-2 (nth key 0)
+            :academic-year-1 (dec (nth key 1))
+            :academic-year-2 (nth key 1)
+            :need-1 need-1
+            :setting-1 setting-1
+            :need-2 need-2
+            :setting-2 setting-2
+            :transition-count transition-count
+            :simulation simulation}))
+       transitions))
+
+(defn add-simulation-idx [simulation-idx sd]
+  (assoc sd :simulation simulation-idx))
+
+(def ->dataset-seq-xf
+  (comp
+   (map (fn [[simulation-idx simulation-data]]
+          (map (partial add-simulation-idx simulation-idx) simulation-data)))
+   (map (fn [simulation]
+          (as-> simulation $
+            (filter :transitions $)
+            (mapcat unpack-transition-counts $)
+            (tc/dataset $)
+            (tc/map-columns $ :need-1 [:need-1] name)
+            (tc/map-columns $ :need-2 [:need-2] name)
+            (tc/map-columns $ :setting-1 [:setting-1] name)
+            (tc/map-columns $ :setting-2 [:setting-2] name)
+            (tc/convert-types $ {:academic-year-1  :int16
+                                 :academic-year-2  :int16
+                                 :calendar-year    :int16
+                                 :calendar-year-2  :int16
+                                 :simulation       :int16
+                                 :transition-count :int32}))))))
+
+(defn output-ds-seq
+  [out-dir prefix ds-seq]
+  (let [start          (System/currentTimeMillis)
+        idx            (-> ds-seq first :simulation first)
+        num-ds         (count ds-seq)
+        _              (println (format "Processing Simulation %d" idx))
+        base-file-name (format "%s-%05d.parquet" prefix idx)
+        _              (println (format "File: %s" base-file-name))
+        file-name      (str out-dir base-file-name)]
+    {:file-name          file-name
+     :number-of-datasets num-ds
+     :status             (parquet/ds-seq->parquet file-name ds-seq)
+     :elapsed-time       (- (System/currentTimeMillis) start)}))
+
+
+(defn create-projections-and-save [{:keys [prefix out-dir config] :as projection-config}
+                                   sim-range]
+  (->> (into []
+             (comp
+              (send/create-projections-xf projection-config
+                                          (:projection-parameters config))
+              ->dataset-seq-xf)
+             sim-range)
+       (output-ds-seq out-dir prefix)))
+
+(defn parallel-create-projections-and-save
+  "Takes a map of
+   - :config the read in config file (required)
+   - :out-dir as a location for the parquet files to be written to (required)
+   - :prefix as a prefix for the parquet files (require)
+   - :standard-projection as the baseline model (required)
+   - :seed-year 1 greater than the max year of the input transitions as the jumping off point (required)
+   - :scenario-projection as the scenario model (optional)
+   - :modify-transitions-data-range years to apply the scenario-projection (optional)
+
+  It then runs the projection in groups of 50 simulations that get
+  written to the same parquet file, so 1000 simulations would create
+  20 parquet files.
+
+  The projection is run in the order that things are completed to best
+  saturate the cores of whatever machine is running the simulation.
+  "
+  [{:keys [config] :as projection-config}]
+  (cp/with-shutdown! [cpu-pool (cp/threadpool (- (cp/ncpus) 2))]
+    (->> (range (get-in config [:projection-parameters :simulations] 1)) ;; Just 1 simulation if nothing is specified
+         (partition-all 50)
+         (lazy/upmap cpu-pool (partial create-projections-and-save projection-config))
+         vec)))
